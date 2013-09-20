b7a1816a89ff64112b788e9445f5fcf845dbf58c
shahrivari <saeed.shahrivari@gmail.com>
2013-09-20 21:02:00 +0330

    Initial project creation.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Saeed
+ * Date: 9/20/13
+ * Time: 10:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
